3fc7a77088363f7e42e9a8581a8adf776ac65050
Stephen Barrett <mail@steviebarrett.com>
2016-06-09 15:00:44 +0100

    Completed Part 3 Coding

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -21,13 +21,37 @@ class APIManager {
         let task = session.dataTaskWithURL(url) {
             (data, response, error) -> Void in
             
-            dispatch_async(dispatch_get_main_queue()) {
-                if error != nil {
+            if error != nil {
+                dispatch_async(dispatch_get_main_queue()) {
                     completion(result: (error!.localizedDescription))
-                } else {
-                    completion(result: "NSURLSession successful")
-                    print(data)
                 }
+            } else {
+                
+                //Added for JSONSerialization
+//                print(data)
+                do {
+                    /* .AllowFragments - top level object is not Array or Dictionary.
+                    Any type of string or value
+                    NSJSONSerialization requires to Do / Try / Catch
+                    Converts the NSDATA into a JSON Object and cast it to a Dictionary */
+                    
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject] {
+                        
+                        print(json)
+                        
+                        let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                completion(result: "JSONSerialization Successful")
+                            }
+                        }
+                    }
+                } catch {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completion(result: "Error in NSJSONSerialization")
+                    }
+                }
+                //End of JSONSerialization
             }
         }
         task.resume()
