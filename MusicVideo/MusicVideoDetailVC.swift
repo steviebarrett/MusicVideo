280ce9ec1f3c4faa1866cf951dab760648945478
steviebarrett <mail@steviebarrett.com>
2016-06-17 11:49:27 +0100

    Completed Part 20

--- MusicVideo/MusicVideoDetailVC.swift
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -18,9 +18,10 @@ class MusicVideoDetailVC: UIViewController {
     @IBOutlet weak var vPrice: UILabel!
     @IBOutlet weak var vRights: UILabel!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MusicVideoDetailVC.preferredFontChange), name: UIContentSizeCategoryDidChangeNotification, object: nil)
         
         title = videos.vArtist
         vName.text = videos.vName
@@ -34,5 +35,17 @@ class MusicVideoDetailVC: UIViewController {
         } else {
             videoImage.image = UIImage(named: "imageNotAvailable")
         }
+    }
+    
+    func preferredFontChange() {
+        vName.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        vPrice.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        vRights.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        vPrice.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+        vGenre.font = UIFont.preferredFontForTextStyle(UIFontTextStyleHeadline)
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
     }
 }
